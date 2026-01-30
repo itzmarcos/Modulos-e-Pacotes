e89546e027bfecb17927f8f7c943c3e8d7182d22
itzmarcos <marcozfuwa@gmail.com>
2026-01-30 07:16:33 -0300

    108

--- 107.py
+++ 107.py
@@ -1,8 +1,8 @@
 import moeda
-
+#ex107,108
 
 p = float(input('Digite o preço: R$'))
-print(f'A metade de {p} é {moeda.metade(p)}')
-print(f'O dobro de {p} é {moeda.dobro(p)}')
-print(f'Aumentando 10%, temos {moeda.aumento(p, 10)}')
-print(f'Reduzindo 15%, temos {moeda.diminuir(p, 15)}')
+print(f'A metade de {moeda.moeda(p)} é {moeda.moeda(moeda.metade(p))}')
+print(f'O dobro de {moeda.moeda(p)} é {moeda.moeda(moeda.dobro(p))}')
+# print(f'Aumentando 10%, temos {moeda.aumento(p, 10)}')
+# print(f'Reduzindo 15%, temos {moeda.diminuir(p, 15)}')
--- moeda.py
+++ moeda.py
@@ -1,11 +1,15 @@
-def metade(p):
+def metade(p=0):
     return p / 2
 
-def dobro(p):
+def dobro(p=0):
     return p * 2
 
-def aumento(p, r=0):
-    return p * (1 + r / 100)
+def aumento(p=0, r=0):
+    return p + (p * r / 100)
+    
+def diminuir(p=0, r=0):
+    return p - (p * r / 100)
 
-def diminuir(p, r=0):
-    return  p * (1 - r / 100)
+
+def moeda(p=0, moeda = 'R$'):
+    return f'{moeda}{p:.2f}'.replace('.', ',')
