4840796ab950b1c322a2fbf7c6757b8a676c92fc
itzmarcos <marcozfuwa@gmail.com>
2026-02-02 17:17:13 -0300

    att

--- 107.py
+++ 107.py
@@ -2,7 +2,7 @@ import moeda
 #ex107,108
 
 p = float(input('Digite o preço: R$'))
-print(f'A metade de {moeda.moeda(p)} é {moeda.moeda(moeda.metade(p))}')
-print(f'O dobro de {moeda.moeda(p)} é {moeda.moeda(moeda.dobro(p))}')
-# print(f'Aumentando 10%, temos {moeda.aumento(p, 10)}')
-# print(f'Reduzindo 15%, temos {moeda.diminuir(p, 15)}')
+print(f'A metade de {moeda.moeda(p)} é {moeda.metade(p, True)}')
+print(f'O dobro de {moeda.moeda(p)} é {moeda.dobro(p, True)}')
+print(f'Aumentando 10%, temos {moeda.aumento(p, 10, True)}')
+print(f'Reduzindo 15%, temos {moeda.diminuir(p, 15, True)}')
--- moeda.py
+++ moeda.py
@@ -1,15 +1,23 @@
-def metade(p=0):
-    return p / 2
+def metade(p=0, f=False):
+    r = p / 2
+    return r if f is False else moeda(r)
 
-def dobro(p=0):
-    return p * 2
 
-def aumento(p=0, r=0):
-    return p + (p * r / 100)
+def dobro(p=0, f=False):
+    r = p * 2
+    return r if not f else moeda(r)
     
-def diminuir(p=0, r=0):
-    return p - (p * r / 100)
+
+def aumento(p=0, t=0, f=False):
+    r = p + (p * t / 100)
+    return r if f is False else moeda(r)
+
+    
+def diminuir(p=0, t=0, f=False):
+    r = p - (p * t / 100)
+    return r if f is False else moeda(r)
 
 
-def moeda(p=0, moeda = 'R$'):
+def moeda(p=0, moeda='R$'):
     return f'{moeda}{p:.2f}'.replace('.', ',')
+
