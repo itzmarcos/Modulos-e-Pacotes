ab0f124685a3e71179e9d1ac82bc7215c5093d00
itzmarcos <marcozfuwa@gmail.com>
2026-02-09 16:55:58 -0300

    all att

--- TratandoErro/menu.py
+++ TratandoErro/menu.py
@@ -1,13 +1,23 @@
 # MENU
 from mods import mod
+from arquivo import arquivos
 from time import sleep
 
+arq = 'cadastro.txt'
+
+if not arquivos.arquivoExiste(arq):
+    arquivos.criarArquivo(arq)
+
 while True:
-    resp = mod.menu(['Cadastrar Pessoas', 'Ver lista de pessoas', 'Sair do Sistima'])
+    resp = mod.menu(['Ver pessoas cadastrada', 'Cadastrar nova pessoas', 'Sair do Sistima'])
     if resp == 1:
         mod.cabeçalho('OPÇÃO 1')
+        arquivos.lerArquivo(arq)
     elif resp == 2:
-        mod.cabeçalho('OPÇÃO 2')
+        mod.cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = int(input('Idade: '))
+        arquivos.cadastrar(arq, nome, idade)
     elif resp == 3:
         mod.cabeçalho('Saindo do sistema.. até logo!')
         break
