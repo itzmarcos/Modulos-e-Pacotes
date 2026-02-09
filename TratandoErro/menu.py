999cddc9e67c6f94038d674815df76075f955c4b
itzmarcos <marcozfuwa@gmail.com>
2026-02-09 16:12:20 -0300

    add

--- TratandoErro/menu.py
+++ TratandoErro/menu.py
@@ -1,5 +1,16 @@
 # MENU
 from mods import mod
+from time import sleep
 
-mod.cabeçalho('Sistema')
-
+while True:
+    resp = mod.menu(['Cadastrar Pessoas', 'Ver lista de pessoas', 'Sair do Sistima'])
+    if resp == 1:
+        mod.cabeçalho('OPÇÃO 1')
+    elif resp == 2:
+        mod.cabeçalho('OPÇÃO 2')
+    elif resp == 3:
+        mod.cabeçalho('Saindo do sistema.. até logo!')
+        break
+    else:
+        print('ERROR')
+    sleep(1)
--- TratandoErro/mods/mod.py
+++ TratandoErro/mods/mod.py
@@ -6,5 +6,24 @@ def cabeçalho(msg):
     print(msg.center(42))
     print(lin())
 
-            
-        
+def menu(lista):
+    cabeçalho('MENU PRINCIPAL')
+    c = 1
+    for item in lista:
+        print(f'{c} - {item}')
+        c += 1
+    print(lin())
+    opc = leiaInt('Sua Opção: ')
+    return opc
+
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except(ValueError, TypeError):
+            print('ERROR')
+        except(KeyboardInterrupt):
+            print('ERROR')
+            return 0
+        else:
+            return n
